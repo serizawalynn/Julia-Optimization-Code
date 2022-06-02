b9cbce190cf69933db8500a21dafb4c28bdfa460
serizawalynn <47999341+serizawalynn@users.noreply.github.com>
2022-06-02 09:33:44 -0700

    Add files via upload
    I modified the code starting at line 58.

---
+++ hw25a.jl
@@ -0,0 +1,63 @@
+using LinearAlgebra
+
+using Printf
+
+Q = [5 -1; -1 2];
+b = [1;1];
+
+# set parameters here, for all gradient descent algorithms
+tol = 1e-10;     # tolerance on norm of gradient
+MaxIter = 10000;  # maximum number of iterations of gradient descent
+
+# define function
+function F(x)
+   val = 0.5*x'*Q*x - b'*x + 1
+   return val
+end
+
+# define gradient
+function DF(x)
+   grad = Q*x-b;
+   return grad
+end
+
+# steepest descent algorithm, with constant step size
+# input: 
+#    x0 = initial point, a 2-vector (e.g. x0=[1;2])
+#    alpha = step size. Constant, in this algorithm.
+# output: 
+#    x = final point
+#
+function SteepestDescent(x0,alpha)
+   # setup for steepest descent
+   x = x0;
+   alpha=alpha
+   successflag = false;
+
+   # perform steepest descent iterations
+   for iter = 1:MaxIter
+       Fval = F(x);
+       Fgrad=DF(x)
+       if sqrt(Fgrad'*Fgrad) < tol
+          @printf("Converged after %d iterations, function value %f\n", iter, Fval)
+          successflag = true;
+          break;
+       end
+       # perform steepest descent step
+       x = x - alpha*Fgrad;
+
+   end
+   if successflag == false
+       @printf("Failed to converge after %d iterations, function value %f\n", MaxIter, F(x))
+   end
+   return x,alpha;
+end
+
+x_null=[2;2]
+
+#5a
+alphas=[.01 .05 .1 .5]
+for i in alphas
+   print("For alpha="*string(i))
+   SteepestDescent(x_null,i)
+end
